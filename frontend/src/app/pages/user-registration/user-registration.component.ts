a9b1a72e2bd48f98438f773db51e78ce5ee44d87
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-03-20 23:00:38 -0300

    CDU002 - Criação do Template da Page UserRegistration

--- frontend/src/app/pages/user-registration/user-registration.component.ts
+++ frontend/src/app/pages/user-registration/user-registration.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/interface/user.interface';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
   styleUrls: ['./user-registration.component.scss']
 })
-export class UserRegistrationComponent {
+export class UserRegistrationComponent{
 
 }
