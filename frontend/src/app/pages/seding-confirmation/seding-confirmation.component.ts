669bd1288a7ae43c7142af9225dd81a68311b993
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-03-21 18:56:54 -0300

    CE001 - Criação da Page SedingConfirmation

---
+++ frontend/src/app/pages/seding-confirmation/seding-confirmation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seding-confirmation',
+  templateUrl: './seding-confirmation.component.html',
+  styleUrls: ['./seding-confirmation.component.scss']
+})
+export class SedingConfirmationComponent {
+
+}
