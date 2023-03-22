29a9dfc3a60136fcbfc3452cabfef5b573257f7d
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-03-21 21:34:44 -0300

    LFS002 - Criação do Template do Layout FullScreen

--- frontend/src/app/layout/layout-full-screen/layout-full-screen.component.ts
+++ frontend/src/app/layout/layout-full-screen/layout-full-screen.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-full-screen',
   templateUrl: './layout-full-screen.component.html',
   styleUrls: ['./layout-full-screen.component.scss']
 })
-export class LayoutFullScreenComponent {
+export class LayoutFullScreenComponent implements OnInit {
+
+  constructor(private router: Router){ }
+
+  ngOnInit(): void {
+    this.router.navigate(['']);
+  }
 
 }
