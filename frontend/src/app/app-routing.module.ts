77268049d55d70aea2269aadc4e6cede9c861669
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-03-22 08:26:04 -0300

    DBB003 - Criação da Rota Dashboard no App-Routing-Module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthguardGuard } from './guard/authguard.guard';
 import { LayoutFullScreenComponent } from './layout/layout-full-screen/layout-full-screen.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RedefinePasswordComponent } from './pages/redefine-password/redefine-password.component';
 import { SedingConfirmationComponent } from './pages/seding-confirmation/seding-confirmation.component';
 import { UserRegistrationComponent } from './pages/user-registration/user-registration.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'userRegistration', component: UserRegistrationComponent },
   { path: 'redefinePassword', component: RedefinePasswordComponent },
   { path: 'sedingConfirmation', component: SedingConfirmationComponent },
   { path: 'wm', component: LayoutFullScreenComponent, canActivateChild:[AuthguardGuard], children:[
-    { path: '', redirectTo: 'wm', pathMatch: 'full' }
+    { path: 'wm', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent }
   ]}
 ];
 
