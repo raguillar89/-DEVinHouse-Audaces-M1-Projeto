ac90234b2fc6925a00ae103f9dc7892f46dc72e4
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-03-22 19:34:56 -0300

    SM002 - Criação do Template do SubMenu e Implementação no Header

--- frontend/src/app/components/sub-menu/sub-menu.component.ts
+++ frontend/src/app/components/sub-menu/sub-menu.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-sub-menu',
   templateUrl: './sub-menu.component.html',
   styleUrls: ['./sub-menu.component.scss']
 })
-export class SubMenuComponent {
+export class SubMenuComponent implements OnInit{
 
+  constructor(private router: Router, private service: UserService){ }
+
+  ngOnInit(): void { }
+
+  logout() {
+    localStorage.removeItem('status');
+    localStorage.removeItem('login');
+    localStorage.removeItem('nome');
+    this.service.showMessage('Logout realizado com sucesso!', true);
+    this.router.navigate(['login']);
+  }
 }
