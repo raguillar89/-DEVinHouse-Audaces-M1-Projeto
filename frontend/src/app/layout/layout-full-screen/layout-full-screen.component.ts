882a658b519cd54549f2369a627b7f8b0012fbfa
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-03-24 11:25:29 -0300

    LM002 - Criação do Template da Page ModelList

--- frontend/src/app/layout/layout-full-screen/layout-full-screen.component.ts
+++ frontend/src/app/layout/layout-full-screen/layout-full-screen.component.ts
@@ -11,7 +11,7 @@ export class LayoutFullScreenComponent implements OnInit {
   constructor(private router: Router){ }
 
   ngOnInit(): void {
-    this.router.navigate(['wm/collection']);
+    this.router.navigate(['wm/model']);
   }
 
 }
