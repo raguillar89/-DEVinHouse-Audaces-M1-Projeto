b683360c9e049128464593f2c80fbaff5b4af54f
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-03-30 16:04:25 -0300

    RGH001 - Criação da Page RegisterGetHelp

---
+++ frontend/src/app/pages/get-help/get-help-register/get-help-register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-help-register',
+  templateUrl: './get-help-register.component.html',
+  styleUrls: ['./get-help-register.component.scss']
+})
+export class GetHelpRegisterComponent {
+
+}
