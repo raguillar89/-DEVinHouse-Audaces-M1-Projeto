03a9e099bfcacaa4f23d468e616d181129128e6d
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-03-30 17:46:22 -0300

    EGH004 - Implementação dos Métodos e Validações no Typescript da Page EditGetHelp

--- frontend/src/app/pages/get-help/get-help-edit/get-help-edit.component.ts
+++ frontend/src/app/pages/get-help/get-help-edit/get-help-edit.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { GetHelp } from 'src/app/interface/getHelp.interface';
+import { GetHelpService } from 'src/app/services/getHelp/get-help.service';
 
 @Component({
   selector: 'app-get-help-edit',
   templateUrl: './get-help-edit.component.html',
   styleUrls: ['./get-help-edit.component.scss']
 })
-export class GetHelpEditComponent {
+export class GetHelpEditComponent implements OnInit {
+
+  gethelp: GetHelp = new GetHelp();
+  formGetHelp!: FormGroup;
+
+  constructor(private service: GetHelpService, private router: Router, private route: ActivatedRoute, private fB: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.gethelp.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+    this.createForm();
+  }
+
+  createForm() {
+    this.formGetHelp = this.fB.group({
+      getHelpTitle: ['', [Validators.required, Validators.minLength(10)]],
+      getHelpText: ['', [Validators.required, Validators.minLength(20)]]
+    });
+  }
+
+  findById():void {
+    this.service.findById(this.gethelp.id).subscribe(gethelp => {
+      this.gethelp = gethelp;
+    });
+  }
+
+  update(): void {
+    this.gethelp.getHelpText = this.formGetHelp.value.getHelpText;
+    this.gethelp.getHelpTitle = this.formGetHelp.value.getHelpTitle;
+
+    if(this.formGetHelp.valid) {
+      this.service.update(this.gethelp).subscribe(() => {
+        this.service.showMessage('Card Atualizada com Sucesso!', true);
+        this.router.navigate(['wm/getHelp']);
+      })
+    } else {
+      this.service.showMessage('Preencha todos os campos!', true);
+    }
+  }
+
+  delete(): void {
+    if(this.formGetHelp.valid) {
+      this.service.delete(this.gethelp.id).subscribe(() => {
+        this.service.showMessage('Card Excluído com Sucesso!', true);
+        this.router.navigate(['wm/getHelp']);
+      })
+    } else {
+      this.service.showMessage('Preencha todos os campos!', true);
+    }
+  }
 
 }
