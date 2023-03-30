e84fe7676bd63a6a4b0f2a7759f1566378ef1ef2
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-03-29 23:45:14 -0300

    UE002 - Criação do Template da Page UserEdit

--- frontend/src/app/pages/user/user-edit/user-edit.component.ts
+++ frontend/src/app/pages/user/user-edit/user-edit.component.ts
@@ -12,7 +12,7 @@ import { UserService } from 'src/app/services/user/user.service';
 export class UserEditComponent implements OnInit {
 
   user: User = new User();
-  formUserCreate!: FormGroup;
+  formUserEdit!: FormGroup;
   listUsers: User[] = [];
   passwordPattern = '^[0-9]+$';
 
@@ -26,7 +26,7 @@ export class UserEditComponent implements OnInit {
   constructor(private router: Router, private service: UserService, private fB: FormBuilder, private route: ActivatedRoute) { }
 
   createForm() {
-    this.formUserCreate = this.fB.group({
+    this.formUserEdit = this.fB.group({
       userName: ['', [Validators.required, Validators.minLength(5)]],
       userCompany: ['', [Validators.required, Validators.minLength(4)]],
       userCnpj: ['', [Validators.required, Validators.minLength(10)]],
@@ -38,28 +38,30 @@ export class UserEditComponent implements OnInit {
   getUsersList() {
     this.service.findAll().subscribe((users) => {
       this.listUsers = users;
+      this.formUserEdit.patchValue(this.user);
     });
   }
 
   findById(): void {
     this.service.findById(this.user.id).subscribe(user => {
       this.user = user;
-      this.formUserCreate.patchValue(this.user);
+      this.formUserEdit.patchValue(this.user);
     });
   }
 
   update(): void {
-    let password = this.formUserCreate.get('userPassword')?.value;
-    let passwordConfirmation = this.formUserCreate.get('userPasswordConfirmation')?.value;
+    let password = this.formUserEdit.get('userPassword')?.value;
+    let passwordConfirmation = this.formUserEdit.get('userPasswordConfirmation')?.value;
 
-    this.user.userName = this.formUserCreate.value.userName;
-    this.user.userCompany = this.formUserCreate.value.userCompany;
-    this.user.userCnpj = this.formUserCreate.value.userCnpj;
+    this.user.userName = this.formUserEdit.value.userName;
+    this.user.userCompany = this.formUserEdit.value.userCompany;
+    this.user.userCnpj = this.formUserEdit.value.userCnpj;
+    this.user.userPassword = this.formUserEdit.value.userPassword;
 
     if(password !== passwordConfirmation){
       this.service.showMessage('Senhas divergentes, favor conferir.', true);
     } else {
-      if(!this.formUserCreate.valid){
+      if(this.formUserEdit.valid){
         this.service.showMessage('Preencha todas as informações', true);
       } else {
         this.service.update(this.user).subscribe(() => {
